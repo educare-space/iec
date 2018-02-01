da4abd455269ae83649454f3f6275e60b90e0154
Rio Purnomo <siriokun@gmail.com>
2018-02-02 01:33:58 +0700

    Bootstrap 4

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <style>
+        .jumbotron {
+            background-color: #dae6ff;
+            border-top: 3px solid #00539b;
+            border-radius: 0;
+            color: #75a0fd;
+        }
+
+        .jumbotron-heading {
+            color: #00539b;
+            text-transform: uppercase;
+            margin: 3rem 0;
+        }
+    </style>
+
+    <title>Islamic Educare</title>
+</head>
+
+<body>
+
+    <section class="jumbotron text-center">
+        <div class="container">
+            <h1 class="jumbotron-heading">Islamic Educare</h1>
+            <p class="lead text-muted">Komunitas sosial dan pendidikan yang bertujuan membentuk dan mengembangkan generasi muda muslim Indonesia yang
+                profesional, beriman, dan berakhlak mulia.</p>
+        </div>
+    </section>
+    <p class="text-center">Islamic Educare - Jl. Sekeloa Timur No. 38</p>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
