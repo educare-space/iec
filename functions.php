f8496a1466393ea26b798e3817a2777d1c11891c
Rio Purnomo <siriokun@gmail.com>
2018-02-03 15:00:35 +0700

    Remove unused scripts

---
+++ functions.php
@@ -0,0 +1,49 @@
+<?php
+add_theme_support( 'title-tag' );
+function disable_wp_emojicons() {
+
+    // all actions related to emojis
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+  
+        // Remove the REST API endpoint.
+      remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+       
+      // Turn off oEmbed auto discovery.
+      add_filter( 'embed_oembed_discover', '__return_false' );
+       
+      // Don't filter oEmbed results.
+      remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+       
+      // Remove oEmbed discovery links.
+      remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+       
+      // Remove oEmbed-specific JavaScript from the front-end and back-end.
+      remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+       
+      // Remove all embeds rewrite rules.
+      // add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+  
+    // filter to remove TinyMCE emojis
+    add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+    add_filter( 'emoji_svg_url', '__return_false' );
+  }
+  add_action( 'init', 'disable_wp_emojicons' );
+  
+  function disable_emojicons_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+      return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+      return array();
+    }
+  }
+  
+  function remove_unused_scripts(){
+    wp_deregister_script( 'wp-embed' );
+  }
+  add_action( 'wp_footer', 'remove_unused_scripts' );
